11f8c2eb8cb40413b81092e2c8be18a9afe832b3
gram <git@orsinium.dev>
2024-03-29 12:09:37 +0100

    test animation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use embedded_graphics::pixelcolor::Rgb888;
 use embedded_graphics::prelude::*;
-use embedded_graphics::primitives::{Circle, PrimitiveStyle};
-use embedded_graphics_simulator::{OutputSettingsBuilder, SimulatorDisplay, Window};
+use embedded_graphics::primitives::{Circle, PrimitiveStyle, StyledDrawable};
+use embedded_graphics_simulator::{
+    OutputSettingsBuilder, SimulatorDisplay, SimulatorEvent, Window,
+};
 
 const WIDTH: usize = 320;
 const HEIGHT: usize = 240;
@@ -10,9 +12,6 @@ fn main() -> Result<(), core::convert::Infallible> {
     let size = Size::new(WIDTH as u32, HEIGHT as u32);
     let mut display = SimulatorDisplay::<Rgb888>::new(size);
     let line_style = PrimitiveStyle::with_stroke(Rgb888::WHITE, 1);
-    Circle::new(Point::new(72, 8), 48)
-        .into_styled(line_style)
-        .draw(&mut display)?;
 
     let output_settings = OutputSettingsBuilder::new()
         .scale(4)
@@ -21,7 +20,16 @@ fn main() -> Result<(), core::convert::Infallible> {
         .max_fps(120)
         .build();
     let mut window = Window::new("Firefly emulator", &output_settings);
-    window.show_static(&display);
-
-    Ok(())
+    let mut i = 0;
+    loop {
+        let circle = Circle::new(Point::new(72 + i, 8), 48);
+        circle.draw_styled(&line_style, &mut display)?;
+        i += 1;
+        window.update(&display);
+        for event in window.events() {
+            if event == SimulatorEvent::Quit {
+                return Ok(());
+            }
+        }
+    }
 }
