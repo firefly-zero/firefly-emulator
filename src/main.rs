ecbf0d72f1da699a92978251b7e312ff121fd282
gram <git@orsinium.dev>
2024-03-29 17:40:36 +0100

    working emulator

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use embedded_graphics::pixelcolor::Rgb888;
 use embedded_graphics::prelude::*;
-use embedded_graphics::primitives::{Circle, PrimitiveStyle, StyledDrawable};
 use embedded_graphics_simulator::{
     OutputSettingsBuilder, SimulatorDisplay, SimulatorEvent, Window,
 };
@@ -10,8 +9,15 @@ const HEIGHT: usize = 240;
 
 fn main() -> Result<(), core::convert::Infallible> {
     let size = Size::new(WIDTH as u32, HEIGHT as u32);
-    let mut display = SimulatorDisplay::<Rgb888>::new(size);
-    let line_style = PrimitiveStyle::with_stroke(Rgb888::WHITE, 1);
+    let display = SimulatorDisplay::<Rgb888>::new(size);
+    let device = firefly_runtime::Device {
+        display,
+        timer: firefly_hosted::Timer::new(),
+        input: firefly_hosted::Input::new(),
+        storage: firefly_hosted::Storage::new(".."),
+        reader: std::marker::PhantomData,
+    };
+    let mut runtime = firefly_runtime::Runtime::new(device, "zig-triangle").unwrap();
 
     let output_settings = OutputSettingsBuilder::new()
         .scale(4)
@@ -20,12 +26,10 @@ fn main() -> Result<(), core::convert::Infallible> {
         .max_fps(120)
         .build();
     let mut window = Window::new("Firefly emulator", &output_settings);
-    let mut i = 0;
+    runtime.start().unwrap();
     loop {
-        let circle = Circle::new(Point::new(72 + i, 8), 48);
-        circle.draw_styled(&line_style, &mut display)?;
-        i += 1;
-        window.update(&display);
+        runtime.update().unwrap();
+        window.update(runtime.display());
         for event in window.events() {
             if event == SimulatorEvent::Quit {
                 return Ok(());
