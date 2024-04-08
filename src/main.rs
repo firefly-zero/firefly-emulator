6bb35797d3ffc25499f79da49ebca1c979b5fc51
gram <git@orsinium.dev>
2024-04-08 16:16:37 +0200

    go: DrawSubImage

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use firefly_emulator::run_emulator;
 
 fn main() {
-    let res = run_emulator("demo", "go-image");
+    let res = run_emulator("demo", "go-sprite");
     if let Err(err) = res {
         println!("{err}");
         std::process::exit(1)
