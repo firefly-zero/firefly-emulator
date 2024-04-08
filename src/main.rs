bbf5595bbb001f755c3c6c29c86cd6395eb40203
gram <git@orsinium.dev>
2024-04-08 12:20:03 +0200

    resolve files relative to the project, migrate demos to config

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use firefly_emulator::run_emulator;
 
 fn main() {
-    let res = run_emulator("demo", "go-touchpad");
+    let res = run_emulator("demo", "go-text");
     if let Err(err) = res {
         println!("{err}");
         std::process::exit(1)
