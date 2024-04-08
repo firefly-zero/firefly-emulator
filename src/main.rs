d9a66711fca25a7f657dd2836930bc96fc4cfa88
gram <git@orsinium.dev>
2024-04-08 15:11:50 +0200

    go: DrawImage

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use firefly_emulator::run_emulator;
 
 fn main() {
-    let res = run_emulator("demo", "go-text");
+    let res = run_emulator("demo", "go-image");
     if let Err(err) = res {
         println!("{err}");
         std::process::exit(1)
