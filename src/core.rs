dd7c60f5c8dedfae246a476f3599887870b5478b
gram <git@orsinium.dev>
2024-04-11 14:54:56 +0200

    emulator: autodetect the last installed app

--- src/core.rs
+++ src/core.rs
@@ -8,15 +8,19 @@ use embedded_graphics_simulator::{
 use firefly_device::DeviceImpl;
 use std::path::PathBuf;
 
-pub fn run_emulator(author_id: &str, app_id: &str) -> Result<(), Error> {
+pub fn run_emulator() -> Result<(), Error> {
     let size = Size::new(
         firefly_runtime::WIDTH as u32,
         firefly_runtime::HEIGHT as u32,
     );
     let display = SimulatorDisplay::<Rgb888>::new(size);
     let vfs_path = get_vfs_path();
+
+    let meta_raw = std::fs::read(vfs_path.join("sys").join("new-app")).unwrap();
+    let meta = firefly_meta::ShortMeta::decode(&meta_raw).unwrap();
+
     let device = DeviceImpl::new(vfs_path);
-    let mut runtime = firefly_runtime::Runtime::new(device, display, author_id, app_id)?;
+    let mut runtime = firefly_runtime::Runtime::new(device, display, meta.author_id, meta.app_id)?;
 
     let output_settings = OutputSettingsBuilder::new()
         .scale(4)
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use firefly_emulator::run_emulator;
 
 fn main() {
-    let res = run_emulator("demo", "go-sprite");
+    let res = run_emulator();
     if let Err(err) = res {
         println!("{err}");
         std::process::exit(1)
