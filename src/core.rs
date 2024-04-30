7c6cadb60231b36becf5edac4b8048509add1f34
gram <git@orsinium.dev>
2024-04-30 12:05:06 +0200

    emulator: handle keyboard input

--- src/core.rs
+++ src/core.rs
@@ -2,10 +2,11 @@ use crate::error::Error;
 use directories::ProjectDirs;
 use embedded_graphics::pixelcolor::Rgb888;
 use embedded_graphics::prelude::*;
+use embedded_graphics_simulator::sdl2::Keycode;
 use embedded_graphics_simulator::{
     OutputSettingsBuilder, SimulatorDisplay, SimulatorEvent, Window,
 };
-use firefly_device::DeviceImpl;
+use firefly_device::{DeviceImpl, Pad};
 use firefly_runtime::*;
 use std::path::PathBuf;
 
@@ -40,7 +41,11 @@ pub fn run_emulator() -> Result<(), Error> {
     Ok(())
 }
 
-fn run_app(window: &mut Window, config: Config) -> Result<Option<Config>, Error> {
+fn run_app(window: &mut Window, mut config: Config) -> Result<Option<Config>, Error> {
+    let mut input = firefly_device::InputState::default();
+    // Reset input in case it is preserved from the previous runtime.
+    config.device.update_input(input.clone());
+
     let mut runtime = Runtime::new(config)?;
     runtime.start()?;
     loop {
@@ -52,15 +57,86 @@ fn run_app(window: &mut Window, config: Config) -> Result<Option<Config>, Error>
         }
         window.update(runtime.display());
         for event in window.events() {
-            // ESC is pressed. Exit the emulator.
-            if event == SimulatorEvent::Quit {
-                return Ok(None);
+            match event {
+                SimulatorEvent::KeyDown { keycode, .. } => {
+                    handle_key_down(keycode, &mut input);
+                }
+                SimulatorEvent::KeyUp { keycode, .. } => {
+                    handle_key_up(keycode, &mut input);
+                }
+                // ESC is pressed. Exit the emulator.
+                SimulatorEvent::Quit => {
+                    return Ok(None);
+                }
+                _ => {}
             }
         }
+        runtime.device_mut().update_input(input.clone());
     }
 }
 
-pub(crate) fn get_vfs_path() -> PathBuf {
+/// A key on the keyboard is released, update the input.
+fn handle_key_up(keycode: Keycode, input: &mut firefly_device::InputState) {
+    match keycode {
+        Keycode::Z | Keycode::Return => input.buttons[0] = false,
+        Keycode::X => input.buttons[1] = false,
+        Keycode::A => input.buttons[2] = false,
+        Keycode::S => input.buttons[3] = false,
+        Keycode::Tab => input.buttons[4] = false,
+        Keycode::Left => {
+            if let Some(pad) = input.pad.as_mut() {
+                pad.x = 0
+            }
+        }
+        Keycode::Right => {
+            if let Some(pad) = input.pad.as_mut() {
+                pad.x = 0
+            }
+        }
+        Keycode::Up => {
+            if let Some(pad) = input.pad.as_mut() {
+                pad.y = 0
+            }
+        }
+        Keycode::Down => {
+            if let Some(pad) = input.pad.as_mut() {
+                pad.y = 0
+            }
+        }
+        _ => {}
+    }
+}
+
+/// A key on the keyboard is pressed, update the input.
+fn handle_key_down(keycode: Keycode, input: &mut firefly_device::InputState) {
+    match keycode {
+        Keycode::Z | Keycode::Return => input.buttons[0] = true,
+        Keycode::X => input.buttons[1] = true,
+        Keycode::A => input.buttons[2] = true,
+        Keycode::S => input.buttons[3] = true,
+        Keycode::Tab => input.buttons[4] = true,
+        Keycode::Left => match input.pad.as_mut() {
+            Some(pad) => pad.x = -1000,
+            None => input.pad = Some(Pad { x: -1000, y: 0 }),
+        },
+        Keycode::Right => match input.pad.as_mut() {
+            Some(pad) => pad.x = 1000,
+            None => input.pad = Some(Pad { x: 1000, y: 0 }),
+        },
+        Keycode::Up => match input.pad.as_mut() {
+            Some(pad) => pad.y = 1000,
+            None => input.pad = Some(Pad { x: 0, y: 1000 }),
+        },
+        Keycode::Down => match input.pad.as_mut() {
+            Some(pad) => pad.y = -1000,
+            None => input.pad = Some(Pad { x: 0, y: -1000 }),
+        },
+        _ => {}
+    }
+}
+
+/// Get path to the virtual file system.
+fn get_vfs_path() -> PathBuf {
     match ProjectDirs::from("com", "firefly", "firefly") {
         Some(dirs) => dirs.data_dir().to_owned(),
         None => PathBuf::from(".firefly"),
