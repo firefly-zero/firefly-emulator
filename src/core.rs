0b09fbc8edac4dc9ac0d6432b309369fadbe0f75
gram <git@orsinium.dev>
2026-09-02 09:31:34 +0200

    finalize config when exiting the emulator

--- src/core.rs
+++ src/core.rs
@@ -94,12 +94,14 @@ fn run_app<'a>(
             return Ok(Some(config));
         }
         if !window.is_open() {
-            runtime.finalize()?;
+            let config = runtime.finalize()?;
+            config.finalize();
             return Ok(None);
         }
         if keyboard {
             if window.is_key_down(Key::Escape) {
-                runtime.finalize()?;
+                let config = runtime.finalize()?;
+                config.finalize();
                 return Ok(None);
             }
             let input = read_keys(window);
